0cb4cbcb2b91718ed560af4f0dbfde12c14b393d
GailDev10 <GailDev10@gmail.com>
2022-07-26 21:29:30 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// Create a server.js file
+//install packages needed to make our app function (npm init)
+//install dependencies (express mongoose ejs dotenv cors)
+//install nodemon (helps our server refresh itself) / but install it as a “dev only” tool [npm install nodemon --save-dev]
+//start: nodemon server.js [ this will allow you to type npm start and have nodemon run your server]
+
+//to make use of express
+const express = require("express");
+
+//to use the app keyword:
+const app = express();
